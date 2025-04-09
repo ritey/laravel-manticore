61f6735d6571d90a4b6a198aacc540b94deb0914
David Wright <dave@ritey.com>
2025-04-09 14:08:29 +0100

    Update test

--- tests/ServiceProviderTest.php
+++ tests/ServiceProviderTest.php
@@ -2,13 +2,24 @@
 
 namespace Tests;
 
-use Orchestra\Testbench\TestCase;
-use Ritey\LaravelManticore\ManticoreServiceProvider;
 use Laravel\Scout\EngineManager;
+use Orchestra\Testbench\TestCase;
 use Ritey\LaravelManticore\ManticoreEngine;
+use Ritey\LaravelManticore\ManticoreServiceProvider;
 
+/**
+ * @internal
+ *
+ * @coversNothing
+ */
 class ServiceProviderTest extends TestCase
 {
+    public function testBootRegistersEngine()
+    {
+        $engine = resolve(EngineManager::class)->engine('manticore');
+        $this->assertInstanceOf(ManticoreEngine::class, $engine);
+    }
+
     protected function getPackageProviders($app)
     {
         return [ManticoreServiceProvider::class];
@@ -19,13 +30,5 @@ class ServiceProviderTest extends TestCase
         $app['config']->set('scout.driver', 'manticore');
         $app['config']->set('laravel_manticore.host', '127.0.0.1');
         $app['config']->set('laravel_manticore.port', 9308);
-    }
-
-    public function testBootRegistersEngine()
-    {
-        $this->app->make('config')->set('scout.driver', 'manticore');
-        $this->app->make(ManticoreServiceProvider::class)->boot();
-        $engine = resolve(EngineManager::class)->engine('manticore');
-        $this->assertInstanceOf(ManticoreEngine::class, $engine);
     }
 }
